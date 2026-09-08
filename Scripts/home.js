6c38937109e0c45e1920513d6c57a0a556e08a28
Harikrishna Lodhi <hklodhi2904@gmail.com>
2026-09-08 16:07:59 +0530

    home page completed

--- Scripts/home.js
+++
@@ -1,6 +0,0 @@
-let guestBtn = document.querySelector('.pop-up button');
-let popUp = document.querySelector('.pop-up-container');
-
-guestBtn.addEventListener('click', () => {
-  popUp.classList.add('clean');
-});
